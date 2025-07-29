5812ae67ad654a5d47f37490425297b71e33a9f7
Dmitry <123327687+0dmitry0@users.noreply.github.com>
2025-07-29 17:36:06 +0300

    Update irule.tcl

--- irule.tcl
+++ irule.tcl
@@ -1,7 +1,7 @@
 when RULE_INIT {
-    set json_response_blocked "{\"status\":\"error\"}"
-    set json_response_permited "{\"status\":\"good\"}"
-    set rps_value 1
+    set ::json_response_blocked "{\"status\":\"error\"}"
+    set ::json_response_permited "{\"status\":\"good\"}"
+    set ::rps_value 1
 }
 
 when HTTP_REQUEST {
@@ -13,14 +13,14 @@ when HTTP_REQUEST {
         
         table set -subtable "rate_limit_$client_ip" "last_time_request" [clock seconds] 120
         
-        log local0.error "Last request time for $client_ip: $last_request"
+        log local0.error "Last request time for $client_ip: $::last_request"
         
         if {$last_request ne "" && [clock seconds] - $last_request < $rps_value} {
             log local0.error "Rate limit exceeded for $client_ip"
-            HTTP::respond 200 content $json_response_blocked "Content-Type" "application/json"
+            HTTP::respond 200 content $::json_response_blocked "Content-Type" "application/json"
             return
         } else {
-            HTTP::respond 200 content $json_response_permited "Content-Type" "application/json"
+            HTTP::respond 200 content $::json_response_permited "Content-Type" "application/json"
             return
         }
     }
